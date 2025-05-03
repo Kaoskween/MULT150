c4c5609b7a2e9e955da255542fc468fc5ca39aa2
Kaoskween <kgomes@san.rr.com>
2025-05-02 19:28:31 -0700

    10 lamps in a line

--- KGomes_Hour11/Assets/Prefabs/Scripts/PrefabGenerator.cs
+++ KGomes_Hour11/Assets/Prefabs/Scripts/PrefabGenerator.cs
@@ -5,10 +5,17 @@ using UnityEngine;
 public class PrefabGenerator : MonoBehaviour
 {
     public GameObject prefab;
+    public int itemCount = 10;
+    public Vector3 startPosition;
+    public float spacing;
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (itemCount = 10; itemCount > 0; itemCount--)
+        {
+            Vector3 currentPosition = startPosition + Vector3.right * (itemCount * spacing);
+            GameObject newObject = Instantiate(prefab, currentPosition, Quaternion.identity);
+        }
     }
 
     // Update is called once per frame
