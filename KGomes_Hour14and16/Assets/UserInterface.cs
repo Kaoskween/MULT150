e224b8ebaae040cab4ea5667ee10e9e7f8b5c00d
Kaoskween <kgomes@san.rr.com>
2025-05-08 17:52:29 -0700

    Hour 14 TIY and my own title screen

---
+++ KGomes_Hour14and16/Assets/UserInterface.cs
@@ -0,0 +1,6 @@
+/* New background image of storm clouds.
+ * New text of game concept I made added.
+ * Text color changed to light blue.
+ * font size increased and font style changed to SC.
+ * Button is now bigger, has new text, starts as blue, changes to red when hovered over, and purple when clicked.
+ Text font changed to Roboto. */
