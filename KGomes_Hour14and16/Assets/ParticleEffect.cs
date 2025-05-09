2062c5ccfee4fcf5bf5a39b7aceb2e961e8f3598
Kaoskween <kgomes@san.rr.com>
2025-05-08 19:27:53 -0700

    Hour 14 and 16 with simple custom particles

---
+++ KGomes_Hour14and16/Assets/ParticleEffect.cs
@@ -0,0 +1,5 @@
+/* Changed emitted shape to Donut.
+ * Changed emitted color to pink.
+ * Decreased emitted rate over time.
+ * Added a point light to the particles.
+ * Added a small trail to the particles.*/
