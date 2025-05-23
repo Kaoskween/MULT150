5db634311bce6ac4c1a4020d3c8650940a9764cc
Kaoskween <kgomes@san.rr.com>
2025-05-23 14:56:31 -0700

    Gauntlet Runner and the changes made to it.

---
+++ KGomes_Hour20/Assets/Scripts/GameCritic.cs
@@ -0,0 +1,7 @@
+/*1. The game is not too easy or hard in my opinion. The game is easy because it is easy to get the powerups and get out of the way of the obstacles without ever having to phase out, but it can be a bit hard since sometimes you just don't move fast enough to get a powerup on the other side of the screen.
+ * 2. I would try to make it a little easier by making the player move from side to side faster. I would try to make it a little harder by making the powerups and obstacles either a little faster or having more obstacles.
+ * 3. The two wow factors I find is the visuals of the game and the effects of the powerup.
+ * 4. Getting the powerups and using the phase state is very fun.
+ * 5. The slow down from hitting obstacles and how long you can play the game can be a bit tedious.
+ * Changed size and color of the obstacle.
+ * Changed color of powerup and the particles. Increased the lenght of the stretched billboard renderer.*/
