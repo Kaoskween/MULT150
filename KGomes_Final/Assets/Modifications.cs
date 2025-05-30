3d169ac25986f1b8ecb1a7708e5976b1d1a0d810
Kaoskween <kgomes@san.rr.com>
2025-05-30 13:29:18 -0700

    Final finished

---
+++ KGomes_Final/Assets/Modifications.cs
@@ -0,0 +1,9 @@
+/*Changed the intensity and orientation of the directional light
+ * Added fog
+ * Added new Skybox and changed the sun size, convergence, and atmoshphere thickness
+ *  converted a chaos ball into a prefab, and replaced all of them in the scene with instances of that prefab. added particle system to the prefab and customised start color, speed, and lifetime, shape, and render mode
+ * Made blue ball the prefab, made duplicates to be the other colors, made new texture and added it. Also added lights to the colored balls.
+ * Increased goal light intensity and range
+ * Made plilars for the wall and a stone texture 
+ * Added donuts and made them work.
+ */
